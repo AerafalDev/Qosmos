710539b96482bb3da74abe2eca73db7abd9749b0
jimmy-mll <jimmy.mll@hotmail.com>
2026-02-04 18:14:34 +0100

    feat: initialize Qosmos application and run asynchronously
    * Set up Qosmos application builder
    * Replace default console output with application run

--- src/Qosmos/Program.cs
+++ src/Qosmos/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Qosmos.Core.Network.Hosting;
 
-Console.WriteLine("Hello, World!");
+var builder = QosmosApplication.CreateBuilder(args);
+
+await builder
+    .Build()
+    .RunAsync();

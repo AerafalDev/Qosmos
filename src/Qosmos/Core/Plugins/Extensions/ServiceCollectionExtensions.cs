e303f2bba87a9561040e1c5d310bd006840d0716
jimmy-mll <jimmy.mll@hotmail.com>
2026-02-04 21:55:01 +0100

    refactor: update AddPlugin method to use PluginWaitingToLoad
    * Change method to register plugin as waiting to load
    * Simplify service registration by using a single instance

--- src/Qosmos/Core/Plugins/Extensions/ServiceCollectionExtensions.cs
+++ src/Qosmos/Core/Plugins/Extensions/ServiceCollectionExtensions.cs
@@ -9,21 +9,19 @@ namespace Qosmos.Core.Plugins.Extensions;
 /// <summary>
 /// Provides extension methods for the <see cref="IServiceCollection"/> to add plugins.
 /// </summary>
-public static class ServiceCollectionExtensions
+internal static class ServiceCollectionExtensions
 {
     /// <summary>
-    /// Adds a plugin and its manifest to the service collection.
+    /// Adds a plugin to the service collection with its manifest and marks it as waiting to load.
     /// </summary>
     /// <typeparam name="T">The type of the plugin, which must inherit from <see cref="Plugin"/>.</typeparam>
     /// <param name="services">The <see cref="IServiceCollection"/> to which the plugin will be added.</param>
     /// <returns>
-    /// The updated <see cref="IServiceCollection"/> with the plugin and its manifest registered.
+    /// The updated <see cref="IServiceCollection"/> with the plugin registered as waiting to load.
     /// </returns>
     public static IServiceCollection AddPlugin<T>(this IServiceCollection services)
         where T : Plugin
     {
-        return services
-            .AddSingleton(PluginManifest.CorePlugin<T>().Build())
-            .AddSingleton<T>();
+        return services.AddSingleton(new PluginWaitingToLoad(string.Empty, PluginManifest.CorePlugin<T>().Build(), true));
     }
 }

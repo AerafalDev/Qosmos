4855f19534a76f2f22bded20154c6a6bed095751
jimmy-mll <jimmy.mll@hotmail.com>
2026-02-04 22:18:06 +0100

    feat: add CodecAttribute for codec marking

---
+++ src/Qosmos/Core/Codecs/Builders/Attributes/CodecAttribute.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Qosmos 2026.
+// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Qosmos.Core.Codecs.Builders.Attributes;
+
+/// <summary>
+/// An attribute used to mark a class as a codec.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class CodecAttribute : Attribute;
